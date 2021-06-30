d11601adf1d6e71d493fc1b57750ca099376cf0d
Oliver Foster <oliver.foster@kineo.com>
2021-06-30 12:23:18 +0100

    Modules

--- .gitmodules.js
+++ .gitmodules.js
@@ -4,6 +4,8 @@ const env = Object.assign({}, process.env, {
   Path: `${process.env.Path};${process.env.PROGRAMFILES}\\Git\\mingw64\\libexec\\git-core;`
 });
 
+console.log(env.path)
+
 ChildProcess.execSync('git submodule update --init', {
   env,
   stdio: 'inherit'

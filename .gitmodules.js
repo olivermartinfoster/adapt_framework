fdd335d5661d84e21dd8c5d06b2abf32df9d46d3
Oliver Foster <oliver.foster@kineo.com>
2021-06-30 12:16:50 +0100

    Preinstall script

---
+++ .gitmodules.js
@@ -0,0 +1,10 @@
+const ChildProcess = require("child_process");
+
+const env = Object.assign({}, process.env, {
+  Path: `${process.env.Path};${process.env.PROGRAMFILES}\\Git\\mingw64\\libexec\\git-core;`
+});
+
+ChildProcess.execSync('git submodule update --init', {
+  env,
+  stdio: 'inherit'
+});
